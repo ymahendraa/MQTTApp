3a4156554d117e23ac219dd6e45f80d3ef62ec71
ymahendraa <ymahendra721@gmail.com>
2021-05-26 19:20:20 +0800

    fix the auth scheme

--- components/loginComponent/index.js
+++ components/loginComponent/index.js
@@ -6,13 +6,12 @@ import {
   Text,
   TextInput,
   Alert,
-  ScrollView
+  ScrollView,
 } from 'react-native';
 import { Button } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import ReactNativeBiometrics from 'react-native-biometrics';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// import {axios} from 'react-native-axios';
 import base64 from 'react-native-base64';
 
 const storeDataUrl = async (value) => {
@@ -29,6 +28,41 @@ const Login = () => {
     const [clientID, setClientID] = useState('');
     const [key, setKey] = useState('');
     const [url, setUrl] = useState('');
+    const [brokerProp, setBrokerProp] = useState({});
+
+    const setBrokerProperties = () => {
+
+    }
+
+    const alertTwoButton = (message) => {
+        Alert.alert(
+            "Login status",
+            message ? "Successfully logged in" : "Login failed, user not registered yet",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => null }
+            ]
+          );
+    }
+
+    const alertBrokerProp = () => {
+        Alert.alert(
+            "Broker Properties",
+            "URL : "+ JSON.stringify(brokerProp.url) + "\n" + "PORT :" + JSON.stringify(brokerProp.port),
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") }
+            ]
+          );
+    }
 
     const getData = async () => {
         try {
@@ -44,12 +78,12 @@ const Login = () => {
         }
     }
 
-    const login = () => {
+    const loginToServer = () => {
         let data = {
             "username" : clientID,
             "password" : key
         }
-        return fetch('https://de6c1f816d7c.ngrok.io/users/authenticate', {
+        return fetch('https://50c94f5921b8.ngrok.io/users/authenticate', {
             method: 'POST',
             headers: {
               'Accept': 'application/json',
@@ -68,30 +102,36 @@ const Login = () => {
 
     const getBroker = () => {
         let headers = new Headers();
-        headers.set('Authorization', 'Basic ' + base64.encode(clientID + ":" + key+'1'));
-        fetch('https://de6c1f816d7c.ngrok.io/users', 
+        headers.set('Authorization', 'Basic ' + base64.encode(clientID + ":" + key));
+        fetch('https://50c94f5921b8.ngrok.io/users', 
         {method:'GET',
         headers: headers,
         })
         .then(response => {
-            if(response.status != 401){
+            if(response.ok){
                 ReactNativeBiometrics.simplePrompt({promptMessage: 'Masukkan sidik jari'})
                 .then((resultObject) => {
-                    const { success } = resultObject
+                    const { success } = resultObject;
                     if (success) {
-                        console.log('successful biometrics provided')
-                        .then(response => response.json())
-                        .then((json) =>{
-                            return console.log(json)
+                        console.log('successful biometrics provided');
+                        response.json()
+                        .then((json) => {
+                            setBrokerProp(json);
+                            console.log(brokerProp)
                         })
+                        alertTwoButton(true)
                     } else {
-                        console.log('user cancelled biometric prompt')
+                        console.log('user cancelled biometric prompt');
                     }
                 })
                 .catch(() => {
-                    console.log('biometrics failed')
+                    console.log('biometrics failed');
                 })
-        }})
+        }else{
+            alertTwoButton(false)
+        }
+        })
+        // .then((response) => response.json())
         // .then((json) => {
         //     return console.log(json);
         // })
@@ -100,10 +140,10 @@ const Login = () => {
         });        
     }
 
-    const mainFunction = async () =>{
+    const mainFunction = async () => {
         try {
-            await getData();
-            getBroker();
+            await getData()
+            .then(getBroker())
             // login();
         } catch (error) {
             
@@ -131,17 +171,25 @@ const Login = () => {
                   <View style={{flexDirection:'row'}}>
                       <Text style={styles.label}>Client ID</Text>
                       <Text style={{marginTop:15, fontWeight:'bold'}}>:</Text>
-                      <TextInput style={styles.textinp} onChangeText={(clientID)=> setClientID(clientID)}  ></TextInput>   
+                      <TextInput style={styles.textinp} onChangeText={(clientID)=> setClientID(clientID)}  onEndEditing={getData}></TextInput>   
                   </View>
                   <View style={{flexDirection:'row'}}>
                       <Text style={styles.label}>Password</Text>
                       <Text style={{marginTop:15, fontWeight:'bold'}}>:</Text>
                       <TextInput style={styles.textinp} onChangeText={(key)=> setKey(key)} ></TextInput>   
                   </View>
-                  <View style={{ flex: 1, alignItems: 'center', marginTop:'20%'}}>
-                    <Button buttonStyle={styles.butlog} title="Login" color="#000" onPress={mainFunction}/>
-                       
+                  <View style={{flexDirection:'row'}}>
+                        <View style={{ flex: 1, alignItems: 'center', marginTop:'5%', marginLeft:'5%'}}>
+                            <Button buttonStyle={styles.butlog} title="Login" color="#000" onPress={mainFunction}/>
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center', marginTop:'5%',marginRight:'5%'}}>
+                            <Button buttonStyle={styles.butlog} title="Broker Prop" color="#000" onPress={alertBrokerProp}/>
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center', marginTop:'5%',marginRight:'5%'}}>
+                            <Button buttonStyle={styles.butlog} title="POST" color="#000" onPress={loginToServer}/>
+                        </View>
                   </View>
+                  
                   <View style={{ flex : 1 }} />
                 </View>    
               </View>
